f0e920095cbc5a6bbe5aacc4c1449c3c3312cef2
ashutosh <amxdp18@gmail.com>
2018-05-17 03:23:24 +0530

    Association between user and Files

--- app/models/doc.rb
+++ app/models/doc.rb
@@ -1,4 +1,5 @@
 class Doc < ApplicationRecord
   validates :title, presence: true
   validates :content, presence: true
+  belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_16_185859) do
+ActiveRecord::Schema.define(version: 2018_05_16_214426) do
 
   create_table "docs", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
