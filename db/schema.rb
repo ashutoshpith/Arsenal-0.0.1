6fdd2c27f0629d04df0639f2b58365765579d0b7
ashutosh <amxdp18@gmail.com>
2018-05-19 03:05:51 +0530

    added mysql2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_16_214426) do
+ActiveRecord::Schema.define(version: 2018_05_18_204718) do
 
   create_table "docs", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.string "title"
